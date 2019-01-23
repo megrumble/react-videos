fd1f7f0c8a3c204af944209c0e94f477915328d6
unknown <megrumble@gmail.com>
2019-01-23 09:18:28 -0500

    ceated videoItem and added onClick

--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -1,9 +1,12 @@
 import './VideoItem.css';
 import React from 'react';
 //destructure video out
-const VideoItem = ({video}) => {
-    return <div className="video-item item">
-        <img className="ui image" alt="medium thumbnail" src={video.snippet.thumbnails.medium.url}/>
+const VideoItem = ({video, onVideoSelect}) => {
+    return <div onClick={() => onVideoSelect(video)} className="video-item item">
+        <img
+            alt={video.snippet.title}
+            className="ui image"
+            src={video.snippet.thumbnails.medium.url}/>
         <div className="content">
             <div className="header">{video.snippet.title}</div>
         </div>
